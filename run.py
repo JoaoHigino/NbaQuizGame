b5ae383d3efc08b21e5cd647f12b845fbbebfd1e
JoaoHigino <jhigino10@hotmail.com>
2022-10-12 13:36:44 +0000

    Add 5 more questions and options

--- run.py
+++ run.py
@@ -106,15 +106,25 @@ questions = {
     "Who has the most assists in a single NBA game?: ": "D",
     "Who has Fastest foul out in nba history?: ": "A",
     "Who has the most Blocks in NBA history?: ": "C",
-    "Who has the most games in NBA history?: ": "B"
+    "Who has the most games in NBA history?: ": "B",
+    "Who has the biggest team winning streak?: ": "B",
+    "Which one was 1st pick on Nba draft?: ": "C",
+    "Who has the best career free throw percent?": "A",
+    "Who was 2008 nba champion?": "C",
+    "Who has the most MVP in nba history?": "B",
+
 }
 # These are the quiz options
 options = [
     ["A. J.Stockton", "B. M.Johnson", "C. L.Wilkens", "D. S.Skiles"],
     ["A. B.Wells", "B. R.Wallace", "C. D.Rodman", "D. A.Gilmore"],
     ["A. D.Mutombo", "B. Kareem", "C. H.Olajuwon", "D. T.Duncan"],
-    ["A. D.Nowitzki", "B. R.Parish", "C. V.Carter", "D. Kareem"]]
-
+    ["A. D.Nowitzki", "B. R.Parish", "C. V.Carter", "D. Kareem"],
+    ["A. GS Warriors", "B. Lakers", "C. Boston Celtics", "D. Chicago Bulls"],
+    ["A. K.Durant", "B. S.Curry", "C. B.Griffin", "D. T.Chandler"],
+    ["A. S.Curry", "B. S.Nash", "C. M.Price", "D. P.Stojaković"],
+    ["A. SA Spurs", "B. Miami Heat", "C. Boston Celtics", "D. Lakers"],
+    ["A. B.Russell", "B. Kareem", "C. W.Chamberlain", "D. M.Jordan"]]
 
 new_game()
 
