d3860ff917705f26f865078d0198672b8d594731
JoaoHigino <jhigino10@hotmail.com>
2022-10-16 19:34:05 +0000

    Add questions and options

--- data.py
+++ data.py
@@ -1,21 +1,26 @@
 QUESTIONS = {
     "Who has the most assists in a single NBA game?: ": "D",
-    "Who has Fastest foul out in nba history?: ": "A",
+    "Who has the Fastest foul out in NBA history?: ": "A",
+    "Who has the most Blocks in NBA history?: ": "C",
+    "Who has the most games played in NBA history?: ": "B",
+    "Who has the most assists in a single NBA game?: ": "D",
+    "Who has the Fastest foul out in NBA history?: ": "A",
     "Who has the most Blocks in NBA history?: ": "C",
     "Who has the most games in NBA history?: ": "B",
     "Who has the biggest team winning streak?: ": "B",
+    "Who has the most turnovers in an NBA game?": "C",
     "Which one was 1st pick on NBA draft?: ": "C",
     "Who has the best career free throw percent?": "A",
-    "Who was 2008 NBA champion?": "C",
+    "Who has the worst career free throw percent?": "D",
+    "Who was the 2008 NBA champion?": "C",
     "Who has the most MVP in NBA history?": "B",
-    "Who is NBA logo?": "C",
+    "Who is the NBA logo?": "C",
     "Which team is not in the Central Division?": "D",
-    "Who was the oldest player to play in a NBA game?": "A",
+    "Who was the oldest player to play in an NBA game?": "A",
     "Which coach has the highest win percentage(min 500 games)?": "D",
     "Where Hakeem 'The Dream' Olajuwon was born?": "B",
-    "Who has is the NBA career leader in personal fouls?": "A",
-    "Who has the most free-throws missed in one game?": "D",
-
+    "Who is the NBA career leader in personal fouls?": "A",
+    "Who has the most free throws missed in one game?": "D",
 
 }
 
@@ -26,8 +31,10 @@ OPTIONS = [
     ["A. D.Mutombo", "B. Kareem", "C. H.Olajuwon", "D. T.Duncan"],
     ["A. D.Nowitzki", "B. R.Parish", "C. V.Carter", "D. Kareem"],
     ["A. GS Warriors", "B. Lakers", "C. Boston Celtics", "D. Chicago Bulls"],
+    ["A. R.Westbrook", "B. G.Arenas", "C. J.Kidd", "D. J.Harden"],
     ["A. K.Durant", "B. S.Curry", "C. B.Griffin", "D. T.Chandler"],
     ["A. S.Curry", "B. S.Nash", "C. M.Price", "D. P.Stojaković"],
+    ["A. S. ONeal", "B. Dennis Rodman", "C. Chris Dudley", "D. Ben Wallace"],
     ["A. SA Spurs", "B. Miami Heat", "C. Boston Celtics", "D. Lakers"],
     ["A. B.Russell", "B. Kareem", "C. W.Chamberlain", "D. M.Jordan"],
     ["A. W.Chamberlain", "B. M.Jordan", "C. J.West", "D. M.Johnson"],
@@ -35,6 +42,7 @@ OPTIONS = [
     ["A. Nat Hickey", "B. Vince Carter ", "C. Kevin Willis", "D. R.Parrish"],
     ["A. Billy Cunningham", "B. K.C. Jones", "C. G.Popovich", "D. P.Jackson"],
     ["A. USA", "B. Nigeria", "C. Cameroon", "D. Ghana"],
-    ["A.  Kareem", "B. R.Parrish", "C. Charles Oakley ", "D. Karl Malone"],
+    ["A. Kareem", "B. R.Parrish", "C. Charles Oakley ", "D. Karl Malone"],
     ["A. W.Chamberlain", "B. DeAndre Jordan", "C. D.Howard", "D. A.Drummond"],
+    
 ]
